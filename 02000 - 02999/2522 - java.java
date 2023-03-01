1d50b32df4420f497df98936cbe50cc6a14eaedc
Han seyoung <20201016@sungshin.ac.kr>
2023-03-01 14:39:11 +0900

    2023-03-01

---
+++ 02000 - 02999/2522 - java.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)  {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < N - i; j++)
+                System.out.printf(" ");
+            for (int j = 0; j < i; j++)
+                System.out.printf("*");
+            System.out.println();
+        }
+
+        for (int i = N - 1; i > 0; i--) {
+            for (int j = 0; j < N - i; j++)
+                System.out.printf(" ");
+            for (int j = 0; j < i; j++)
+                System.out.printf("*");
+            System.out.println();
+        }
+    }
+}
