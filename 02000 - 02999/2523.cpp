110be47ab6daa281524a5325b046beff2003940e
Han seyoung <20201016@sungshin.ac.kr>
2023-03-15 17:27:19 +0900

    2023-03-15

---
+++ 02000 - 02999/2523.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j <= i; j++)
+			printf("*");
+		printf("\n");
+	}
+
+	for (int i = N - 2; i >= 0; i--) {
+		for (int j = 0; j <= i; j++)
+			printf("*");
+		printf("\n");
+	}
+
+	return 0;
+}
