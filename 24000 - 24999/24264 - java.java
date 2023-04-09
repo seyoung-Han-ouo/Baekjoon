f164d926c1a4fd594f0005dbedc0c067b5b404d8
Han seyoung <20201016@sungshin.ac.kr>
2023-04-09 14:45:40 +0900

    2023-04-09

---
+++ 24000 - 24999/24264 - java.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Integer.parseInt(br.readLine()); // 입력의 크기
+
+        System.out.println(n * n + "\n2");
+    }
+}
