d6a882f9a923b4110751309cf39e4c3384567597
Han seyoung <20201016@sungshin.ac.kr>
2023-04-27 21:53:32 +0900

    2023-04-27

---
+++ 11000 - 11999/11365 - java.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String s = br.readLine();
+
+            if (s.equals("END")) break;
+
+            for (int i = s.length() - 1; i >= 0; i--)
+                System.out.print(s.charAt(i));
+            System.out.println();
+        }
+    }
+}
