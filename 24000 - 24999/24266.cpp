436876b7750dc4bf612819811b720dbf6596925b
Han seyoung <20201016@sungshin.ac.kr>
2023-06-28 21:25:16 +0900

    2023-06-28

---
+++ 24000 - 24999/24266.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	long long n;
+	cin >> n;
+
+	cout << n * n * n << "\n3\n";
+
+	return 0;
+}
