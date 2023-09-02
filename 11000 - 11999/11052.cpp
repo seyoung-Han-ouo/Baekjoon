b91d062bccd166299f2580c4f6ea96e22754147d
Han seyoung <20201016@sungshin.ac.kr>
2023-09-02 15:42:37 +0900

    2023-09-02

---
+++ 11000 - 11999/11052.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int P[1001];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N;
+    cin >> N;
+
+    for (int i = 1; i <= N; i++) {
+        cin >> P[i];
+
+        for (int j = 1; j <= i / 2; j++) 
+            P[i] = max(P[i], P[i - j] + P[j]);
+    }
+
+    cout << P[N] << '\n';
+
+    return 0;
+}
