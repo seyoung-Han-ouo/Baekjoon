faa89ec112a16dc499bd5e5e035d4ea2189187e8
Han seyoung <20201016@sungshin.ac.kr>
2024-11-11 23:43:13 +0900

    2024-11-11

---
+++ 32000 - 32999/32384.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    while (N--) cout << "LoveisKoreaUniversity ";
+
+    return 0;
+}
