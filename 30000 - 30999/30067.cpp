f45a6ba5352cea45258a8f156cf7a9b44f975fc2
Han seyoung <20201016@sungshin.ac.kr>
2025-01-23 17:40:57 +0900

    2025-01-23

---
+++ 30000 - 30999/30067.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum = 0;
+
+    for (int i = 0; i < 10; i++) {
+        int n;
+        cin >> n;
+
+        sum += n;
+    }
+
+    cout << sum / 2;
+
+    return 0;
+}
