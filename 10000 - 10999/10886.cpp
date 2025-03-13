1394146f7b380277a9b37c392a30771e67cb8ce6
Han seyoung <20201016@sungshin.ac.kr>
2025-03-13 23:17:44 +0900

    2025-03-13

---
+++ 10000 - 10999/10886.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N, cnt = 0;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		int n;
+		cin >> n;
+
+		cnt += n;
+	}
+
+	if (cnt > N - cnt) 
+		cout << "Junhee is cute!\n";
+	else 
+		cout << "Junhee is not cute!\n";
+
+	return 0;
+}
