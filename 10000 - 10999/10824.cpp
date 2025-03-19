7dac3625a725525b444d3a7fbf6b337349c80698
Han seyoung <20201016@sungshin.ac.kr>
2025-03-20 00:16:21 +0900

    2025-03-20

---
+++ 10000 - 10999/10824.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string A, B, C, D;
+    cin >> A >> B >> C >> D;
+
+    cout << stoull(A + B) + stoull(C + D) << '\n';
+
+    return 0;
+}
