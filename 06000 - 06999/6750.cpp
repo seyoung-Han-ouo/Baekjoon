7d921130cc1561b360977623aa4d589899ea8f20
Han seyoung <20201016@sungshin.ac.kr>
2025-03-21 01:10:22 +0900

    2025-03-21

---
+++ 06000 - 06999/6750.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+char letter[7] = { 'I', 'O', 'S', 'H', 'Z', 'X', 'N' };
+
+bool canUse(char c);
+
+bool canUse(char c) {
+    for (int i = 0; i < 7; i++) {
+        if (c == letter[i]) return true;
+    }
+
+    return false;
+}
+
+int main() {
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < s.length(); i++) {
+        if (!canUse(s[i])) {
+            cout << "NO\n";
+            return 0;
+        }
+    }
+
+    cout << "YES\n";
+
+    return 0;
+}
