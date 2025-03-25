d9810812564155003e39302eb5a29b173af116ad
Han seyoung <20201016@sungshin.ac.kr>
2025-03-26 01:21:37 +0900

    2025-03-26

---
+++ 02000 - 02999/2935.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    string A, B;
+    char c;
+
+    cin >> A >> c >> B;
+
+    if (c == '+') {
+        if (A.length() < B.length()) {
+            string C = A;
+            A = B;
+            B = C;
+        }
+
+        A[A.length() - B.length()] += 1;
+
+        cout << A << '\n';
+    }
+    else {
+        cout << A;
+        for (int i = 1; i < B.length(); i++) cout << 0;
+        cout << "\n";
+    }
+
+    return 0;
+}
