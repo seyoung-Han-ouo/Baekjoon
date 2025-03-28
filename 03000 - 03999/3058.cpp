807c5f288c08ed8badaad5463ecd312c7caa9a14
Han seyoung <20201016@sungshin.ac.kr>
2025-03-29 01:01:32 +0900

    2025-03-28

---
+++ 03000 - 03999/3058.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int sum = 0;
+        int mn = 101;
+
+        for (int i = 0; i < 7; i++) {
+            int n;
+            cin >> n;
+
+            if (n % 2 == 0) {
+                sum += n;
+                mn = min(mn, n);
+            }
+        }
+
+        cout << sum << ' ' << mn << '\n';
+    }
+    
+    return 0;
+}
