821e063deeec9818dc5a955149c19995f4319a16
Han seyoung <20201016@sungshin.ac.kr>
2025-03-29 17:07:55 +0900

    2025-03-29

---
+++ 11000 - 11999/11655.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    string s;
+    getline(cin, s);
+    
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] >= 'a' && s[i] <= 'z') {
+            s[i] += 13;
+
+            if (s[i] < 'a' || s[i] > 'z') s[i] -= 26;
+        }
+        else if (s[i] >= 'A' && s[i] <= 'Z') {
+            s[i] += 13;
+
+            if (s[i] > 'Z') s[i] -= 26;
+        }
+
+        cout << s[i];
+    }
+    
+    return 0;
+}
