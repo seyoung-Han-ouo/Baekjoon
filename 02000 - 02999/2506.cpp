1c55a5f317959f056d447d0f6c19e7eae2346a36
Han seyoung <20201016@sungshin.ac.kr>
2025-04-05 00:34:35 +0900

    2025-04-05

---
+++ 02000 - 02999/2506.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int N;
+    cin >> N;
+
+    int sum = 0;
+    int last = 0;
+
+    while (N--) {
+        int n;
+        cin >> n;
+
+        if (last > 0 && n == 1) n += last;
+        sum += n;
+
+        last = n;
+    }
+
+    cout << sum << '\n';
+
+    return 0;
+}
