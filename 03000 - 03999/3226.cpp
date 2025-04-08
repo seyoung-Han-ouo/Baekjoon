b34ea5213161cee30985a18d033493b1288056bc
Han seyoung <20201016@sungshin.ac.kr>
2025-04-08 15:57:43 +0900

    2025-04-08

---
+++ 03000 - 03999/3226.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int N, cost = 0;;
+    cin >> N;
+
+    while (N--) {
+        string s;
+        int n;
+        cin >> s >> n;
+
+        int time = (s[0] - '0') * 600 + (s[1] - '0') * 60 + (s[3] - '0') * 10 + (s[4] - '0');
+
+        while (n--) {
+            if (420 <= time && time < 1140) cost += 10;
+            else cost += 5;
+
+            time++;
+
+            if (time == 1140) time = 0;
+        }
+    }
+
+    cout << cost << '\n';
+
+    return 0;
+}
