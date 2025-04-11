c8acfa7013ebef237493f55a134b2530e9a73d33
Han seyoung <20201016@sungshin.ac.kr>
2025-04-11 21:45:03 +0900

    2025-04-11

---
+++ 05000 - 05999/5032.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int e, f, c;
+
+int drink(int n);
+
+int drink(int n) {
+    int cnt = 0;
+
+    while (n >= c) {
+        n -= c;
+        n++;
+        cnt++;
+    }
+
+    return cnt;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> e >> f >> c;
+
+    cout << drink(e + f) << '\n';
+
+    return 0;
+}
