e0be1a056cfb480b874f643430e40e7442f7cf8a
Han seyoung <20201016@sungshin.ac.kr>
2025-04-13 19:24:04 +0900

    2025-04-13

---
+++ 17000 - 17999/17283.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int tree(int len, double r);
+
+int tree(int len, double r) {
+    int result = 0;
+    int cnt = 1;
+
+    while (true) {
+        cnt *= 2;
+        len = floor(len * r);
+
+        if (len <= 5) break;
+
+        result += (len * cnt);
+    }
+
+    return result;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int L, R;
+    cin >> L >> R;
+
+    cout << tree(L, R / 100.0) << '\n';
+
+    return 0;
+}
