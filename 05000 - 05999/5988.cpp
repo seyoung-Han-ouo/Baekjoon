595ebbfcec14785ade452125f1b95ea0f61ccd4e
Han seyoung <20201016@sungshin.ac.kr>
2025-04-17 00:41:16 +0900

    2025-04-17

---
+++ 05000 - 05999/5988.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    while (N--) {
+        string s;
+        cin >> s;
+
+        if ((s[s.length() - 1] - '0') % 2 == 0) cout << "even\n";
+        else cout << "odd\n";
+    }
+
+    return 0;
+}
