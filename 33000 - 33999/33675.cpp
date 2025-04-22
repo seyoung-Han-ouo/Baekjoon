404d27218f6fbe192a73190680004ed2af34b86b
Han seyoung <20201016@sungshin.ac.kr>
2025-04-22 16:55:00 +0900

    2025-04-22

---
+++ 33000 - 33999/33675.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        if (N % 2 == 1) cout << "0\n";
+        else {
+            long long cnt = 1;
+            for (int i = 0; i < N / 2; i++) cnt *= 2;
+
+            cout << cnt << '\n';
+        }
+    }
+
+    return 0;
+}
