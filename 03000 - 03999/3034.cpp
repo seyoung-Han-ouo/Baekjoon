16831c335231cd798f96d36c6a7746522abd2c1b
Han seyoung <20201016@sungshin.ac.kr>
2025-04-24 00:13:48 +0900

    2025-04-24

---
+++ 03000 - 03999/3034.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, W, H;
+    cin >> N >> W >> H;
+
+    double d = sqrt(W * W + H * H);
+
+    while (N--) {
+        int n;
+        cin >> n;
+
+        if (n <= W || n <= H || n <= d) cout << "DA\n";
+        else cout << "NE\n";
+    }
+
+    return 0;
+}
