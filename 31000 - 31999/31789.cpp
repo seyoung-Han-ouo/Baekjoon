9e825e3cd32a3552f5e14dd914b2e94ff295f2af
Han seyoung <20201016@sungshin.ac.kr>
2025-04-26 00:41:15 +0900

    2025-04-26

---
+++ 31000 - 31999/31789.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, X, S;
+    cin >> N >> X >> S;
+
+    bool canWin = false;
+
+    while (N--) {
+        int c, p;
+        cin >> c >> p;
+
+        if (c <= X && p > S) canWin = true;
+    }
+
+    if (canWin) cout << "YES\n";
+    else cout << "NO\n";
+
+    return 0;
+}
