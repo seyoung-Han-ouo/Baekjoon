67f9ca0f5db0145081bb05f52d9e4a0d1de3813b
Han seyoung <20201016@sungshin.ac.kr>
2025-05-07 22:45:53 +0900

    2025-05-07

---
+++ 15000 - 15999/15667.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+  
+    int N;
+    cin >> N;
+
+    for (int i = 1; ; i++) {
+        if (i + i * i + 1 == N) {
+            cout << i << '\n';
+            break;
+        }
+    }
+
+    return 0;
+}
