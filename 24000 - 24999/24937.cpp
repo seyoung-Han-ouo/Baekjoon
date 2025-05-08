e2b1194fc207ea545480b020bcced0f371135223
Han seyoung <20201016@sungshin.ac.kr>
2025-05-08 15:09:01 +0900

    2025-05-08

---
+++ 24000 - 24999/24937.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+string s = "SciComLove";
+
+void print(int start, int end);
+
+void print(int start, int end) {
+    for (int i = start; i < end; i++) cout << s[i];
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+    
+    int n = (N % s.length());
+
+    print(n, s.length());
+    print(0, n);
+    cout << '\n';
+
+    return 0;
+}
