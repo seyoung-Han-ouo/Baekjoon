4c3ea82f0fbf53942ff1b14e1a2a01c95f06968d
Han seyoung <20201016@sungshin.ac.kr>
2025-05-27 14:45:05 +0900

    2025-05-27

---
+++ 17000 - 17999/17288.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	string s;
+	cin >> s;
+
+	int result = 0;
+	int cnt = 1;
+
+	for (int i = 1; i < s.length(); i++) {
+		if (s[i - 1] + 1 == s[i]) cnt++;
+		else {
+			if (cnt == 3) result++;
+
+			cnt = 1;
+		}
+	}
+
+	if (cnt == 3) result++;
+
+	cout << result << '\n';
+
+	return 0;
+}
