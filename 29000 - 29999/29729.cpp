acf21eb1fdeee1392f95e43bce7c0e6297b5ba63
Han seyoung <20201016@sungshin.ac.kr>
2025-08-07 22:07:11 +0900

    2025-08-07

---
+++ 29000 - 29999/29729.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int S, N, M;
+	cin >> S >> N >> M;
+
+	int cnt = 0;
+	for (int i = 0; i < N + M; i++) {
+		int n;
+		cin >> n;
+
+		if (n == 0) cnt--;
+		else {
+			if (cnt == S) S *= 2;
+			cnt++;
+		}
+	}
+
+	cout << S << '\n';
+
+	return 0;
+}
