d3efddf51bbb5d0697fcebe7796bb24bd7c0bf3d
Han seyoung <20201016@sungshin.ac.kr>
2025-08-21 22:03:52 +0900

    2025-08-21

---
+++ 10000 - 10999/10874.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+bool check();
+
+bool check() {
+	bool result = true;
+
+	for (int i = 0; i < 10; i++) {
+		int n;
+		cin >> n;
+
+		if (i % 5 + 1 != n) result = false;
+	}
+
+	return result;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		if (check()) cout << i + 1 << '\n';
+	}
+
+	return 0;
+}
