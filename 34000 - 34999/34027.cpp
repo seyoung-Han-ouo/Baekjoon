38e7f584f6485d0197d0b2c3f5cdf393eb961bdd
Han seyoung <20201016@sungshin.ac.kr>
2025-08-22 21:44:48 +0900

    2025-08-22

---
+++ 34000 - 34999/34027.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int T;
+	cin >> T;
+
+	while (T--) {
+		int N;
+		cin >> N;
+
+		int n = sqrt(N);
+
+		cout << (int)(n * n == N) << '\n';
+	}
+
+	return 0;
+}
