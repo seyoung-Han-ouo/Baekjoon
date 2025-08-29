87d081f2f06d986bd61ff1b701878d25a4455c38
Han seyoung <20201016@sungshin.ac.kr>
2025-08-29 17:44:26 +0900

    2025-08-29

---
+++ 02000 - 02999/2896.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	double A, B, C, a, b, c;
+	cin >> A >> B >> C >> a >> b >> c;
+
+	double cnt = min(A / a, min(B / b, C / c));
+
+	cout.precision(7);
+	cout << A - (a * cnt) << ' ' << B - (b * cnt) << ' ' << C - (c * cnt) << '\n';
+
+	return 0;
+}
