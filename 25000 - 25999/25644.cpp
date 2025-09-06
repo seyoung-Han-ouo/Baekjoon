24c3adceef4a6f98e4c5d7129c10b9cd43fe939f
Han seyoung <20201016@sungshin.ac.kr>
2025-09-06 17:38:48 +0900

    2025-09-06

---
+++ 25000 - 25999/25644.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+
+	int result = 0;
+	int mn = 1000000001;
+
+	while (N--) {
+		int n;
+		cin >> n;
+
+		mn = min(n, mn);
+		result = max(n - mn, result);
+	}
+
+	cout << result << '\n';
+
+	return 0;
+}
