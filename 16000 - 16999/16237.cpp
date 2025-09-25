c641e5bb7151d92107ca02da61d7394f637c7d6f
Han seyoung <20201016@sungshin.ac.kr>
2025-09-25 21:25:03 +0900

    2025-09-25

---
+++ 16000 - 16999/16237.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int A, B, C, D, E;
+	cin >> A >> B >> C >> D >> E;
+
+	int cnt = D + E;
+	A -= min(A, D);
+
+	cnt += C;
+	if (B >= C) 
+		B -= C;
+	else {
+		A -= (C - B) * 2;
+		B = 0;
+	}
+
+	if (B > 0) {
+		cnt += B / 2 + B % 2;
+		A -= B / 2 + (B % 2) * 3;
+	}
+
+	if (A > 0) cnt += A / 5 + (bool)(A % 5);
+
+	cout << cnt << '\n';
+
+	return 0;
+}
