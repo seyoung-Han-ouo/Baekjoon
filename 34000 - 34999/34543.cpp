b35e847e64a842536213098df376a22e36eb1038
Han seyoung <20201016@sungshin.ac.kr>
2025-10-01 17:01:50 +0900

    2025-10-01

---
+++ 34000 - 34999/34543.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N, W;
+	cin >> N >> W;
+
+	int score = N * 10;
+
+	if (N >= 3) score += 20;
+	if (N == 5) score += 50;
+	if (W > 1000) score -= 15;
+
+	if (score < 0) cout << "0\n";
+	else cout << score << '\n';
+
+	return 0;
+}
