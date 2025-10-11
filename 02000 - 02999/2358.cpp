7176e6a9588e1c7244178c2b9d3c1645e8032d80
Han seyoung <20201016@sungshin.ac.kr>
2025-10-11 17:37:45 +0900

    2025-10-11

---
+++ 02000 - 02999/2358.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+unordered_map<int, int> x, y;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	while (n--) {
+		int a, b;
+		cin >> a >> b;
+
+		x[a]++;
+		y[b]++;
+	}
+
+	int cnt = 0;
+	for (const auto& pair : x)
+		cnt += (pair.second >= 2);
+
+	for (const auto& pair : y)
+		cnt += (pair.second >= 2);
+
+	cout << cnt << '\n';
+
+	return 0;
+}
