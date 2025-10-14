d14368da725b40c739d213fd6e8109719287c89a
Han seyoung <20201016@sungshin.ac.kr>
2025-10-14 17:21:48 +0900

    2025-10-14

---
+++ 01000 - 01999/1439.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	string S;
+	cin >> S;
+
+	int cnt = 1;
+	char now = S[0];
+
+	for (int i = 1; i < S.length(); i++) {
+		if (now != S[i]) {
+			now = S[i];
+			cnt++;
+		}
+	}
+
+	cout << cnt / 2 << '\n';
+
+	return 0;
+}
