af5c93b8c49a9603ace0153ec4e5a87ab672e379
Han seyoung <20201016@sungshin.ac.kr>
2025-11-03 15:14:21 +0900

    2025-11-03

---
+++ 14000 - 14999/14916.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int findCnt(int n);
+
+int findCnt(int n) {
+	int five = n / 5;
+	n %= 5;
+
+	while (five > 0) {
+		if (n % 2 == 0) return five + n / 2;
+
+		n += 5;
+		five--;
+	}
+
+	if (n % 2 == 0) return five + n / 2;
+
+	return -1;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	cout << findCnt(n) << '\n';
+
+	return 0;
+}
