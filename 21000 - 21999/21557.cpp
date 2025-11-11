b76e93cfa520abf008bd92b71a2a1dba8e4d9d08
Han seyoung <20201016@sungshin.ac.kr>
2025-11-11 15:37:53 +0900

    2025-11-11

---
+++ 21000 - 21999/21557.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int N, arr[200001];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> N;
+    for (int i = 0; i < N; i++) cin >> arr[i];
+
+    for (int i = 0; i < N - 3; i++) {
+        if (arr[0] < arr[N - 1]) arr[N - 1]--;
+        else arr[0]--;
+    }
+
+    cout << max(arr[0], arr[N - 1]) - 1 << '\n';
+
+    return 0;
+}
