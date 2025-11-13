1d4bab8eda6d50f28ef99802bd711042f382474a
Han seyoung <20201016@sungshin.ac.kr>
2025-11-13 21:09:17 +0900

    2025-11-13

---
+++ 34000 - 34999/34722.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    int cnt = 0;
+
+    while (N--) {
+        int s, c, a, r;
+        cin >> s >> c >> a >> r;
+
+        if (s >= 1000 || c >= 1600 || a >= 1500 || (r <= 30 && r != -1)) cnt++;
+    }
+
+    cout << cnt << '\n';
+
+    return 0;
+}
