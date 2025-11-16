94c012e25fb3c2d34e14eb7b4d9daca3df09ce80
Han seyoung <20201016@sungshin.ac.kr>
2025-11-16 14:21:19 +0900

    2025-11-16

---
+++ 32000 - 32999/32290.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+bool arr[1000000];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int l, r, x;
+    cin >> l >> r >> x;
+
+    for (int i = l; i <= r; i++) arr[i | x] = true;
+
+    for (int i = 0; ; i++) {
+        if (!arr[i]) {
+            cout << i << '\n';
+            break;
+        }
+    }
+
+    return 0;
+}
