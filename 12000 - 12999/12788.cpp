477e0ad6f35820c283271a9763221359cbdbc446
Han seyoung <20201016@sungshin.ac.kr>
2025-11-24 16:31:57 +0900

    2025-11-24

---
+++ 12000 - 12999/12788.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[1001];
+
+void findCnt(int len, int need);
+
+void findCnt(int len, int need) {
+    int cnt = 0;
+    int result = 0;
+
+    for (int i = len; i >= 0; i--) {
+        cnt += arr[i];
+        result++;
+
+        if (cnt >= need) {
+            cout << result << '\n';
+            return;
+        }
+    }
+
+    cout << "STRESS\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, M, K;
+    cin >> N >> M >> K;
+
+    for (int i = 0; i < N; i++) cin >> arr[i];
+
+    sort(arr, arr + N);
+
+    findCnt(N - 1, M * K);
+
+    return 0;
+}
