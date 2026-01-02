f3f84340ff0297d0256994433f370c2130f933af
Han seyoung <20201016@sungshin.ac.kr>
2026-01-02 16:32:42 +0900

    2026-01-02

---
+++ 13000 - 13999/13699.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+long long dp[36];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    dp[0] = 1;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < i; j++)
+            dp[i] += dp[j] * dp[i - j - 1];
+    }
+
+    cout << dp[n] << '\n';
+
+    return 0;
+}
