09f2ee816da24928192109942dce7c1bc7a81440
Han seyoung <20201016@sungshin.ac.kr>
2026-02-03 16:40:48 +0900

    2026-02-03

---
+++ 16000 - 16999/16212.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[500001];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) cin >> arr[i];
+
+    sort(arr, arr + N);
+
+    for (int i = 0; i < N; i++) cout << arr[i] << ' ';
+
+    return 0;
+}
