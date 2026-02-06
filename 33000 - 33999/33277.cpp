bfb0faf94514b1a7690a788b476fb440a55f824d
Han seyoung <20201016@sungshin.ac.kr>
2026-02-06 15:48:44 +0900

    2026-02-06

---
+++ 33000 - 33999/33277.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, M;
+    cin >> N >> M;
+
+    int time = ((double)M / N) * 24 * 60;
+
+    cout << setfill('0');
+    cout << std::setw(2) << time / 60 << ':'
+         << std::setw(2) << time % 60 << '\n';
+
+    return 0;
+}
