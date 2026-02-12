608f60f1537d3558a6367ec018fa57d2cc3059d9
Han seyoung <20201016@sungshin.ac.kr>
2026-02-12 16:56:15 +0900

    2026-02-12

---
+++ 14000 - 14999/14912.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, d;
+    cin >> n >> d;
+
+    int cnt = 0;
+
+    for (int i = 1; i <= n; i++) {
+        int num = i;
+
+        while (num > 0) {
+            if (num % 10 == d) cnt++;
+            num /= 10;
+        }
+    }
+
+    cout << cnt << '\n';
+
+    return 0;
+}
